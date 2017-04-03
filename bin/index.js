a7c755ea743c07300eb2ca9b48fd0be8f23239ff
Gergő Móricz <mo.geryy@gmail.com>
2017-04-03 13:16:44 +0200

    Intial code!

---
+++ bin/index.js
@@ -0,0 +1,50 @@
+#! /usr/bin/env node
+var rimraf = require('rimraf');
+var shell = require('shelljs');
+var fs = require('fs');
+const path = require('path');
+var directory = process.cwd();
+
+if(!shell.which('git')){
+	console.log("Sorry, edgemodules requires git.");
+	shell.exit(1);
+}
+
+var modulesExists = fs.existsSync("node_modules/");
+if (modulesExists) {
+	console.log("node_modules/ exists.");
+	var modules = fs.readdirSync("node_modules/").filter(file => fs.statSync(path.join("node_modules/", file)).isDirectory());
+	modules.forEach(function (value, index){
+		var packagePath = path.join("node_modules/", value, "package.json");
+		var packageExists = fs.existsSync(packagePath);
+		console.log(packagePath);
+		if (packageExists){
+			var packageConfig = JSON.parse(fs.readFileSync(packagePath).toString());
+			console.log("Updating " + packageConfig.name + "...");
+			var modulePath = path.join("node_modules/", value);
+			if (typeof packageConfig.repository == "undefined"){
+				console.log("Couldn't update " + packageConfig.name + ", because it doesn't have a repository.");
+			} else if (packageConfig.repository.type != "git"){
+				console.log("Couldn't update " + packageConfig.name + ", because repository type \"" + packageConfig.repository.type + "\" is not supported.");
+			} else {
+				rimraf(modulePath, function(){ //fuck async
+					console.log("Deleted " + packageConfig.name + ".");
+					if (packageConfig.repository.url.startsWith("git+")){
+						packageConfig.repository.url = packageConfig.repository.url.substr("git+".length);
+					}
+					if(shell.exec("git clone " + packageConfig.repository.url + " " + modulePath).code !== 0){
+						console.log("Git clone failed. Aborting.");
+					} else {
+						rimraf(path.join(modulePath, ".git/"), function(){
+							console.log("Successfully updated " + packageConfig.name + "!");
+						});
+					}
+				});
+			}
+		} else {
+			console.log("Couldn't upgrade a package because it's package.json doesn't exist.");
+		}
+	});
+} else {
+	console.log("node_modules/ doesn't exists, aborting.");
+}
